1f9ad7cb1b3ea1f42a96d039c3f9d1efd84b8ce7
sohyoeun <she020108@gmail.com>
2026-04-11 22:52:36 +0900

    fix: extendTailwindMerge에 text-* 커스텀 토큰 등록

--- shared/lib/cn.ts
+++ shared/lib/cn.ts
@@ -1,7 +1,53 @@
 import clsx from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
 
 import type { ClassValue } from 'clsx';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [
+        {
+          text: [
+            'display-base',
+            'display-sm',
+            'heading-2xl',
+            'heading-xl',
+            'heading-lg',
+            'heading-base',
+            'heading-sm',
+            'heading-xs',
+            'body-xl',
+            'body-lg',
+            'body-base',
+            'body-sm',
+            'body-xs',
+            'detail-lg',
+            'detail-base',
+            'detail-sm',
+            'detail-xs',
+          ],
+        },
+      ],
+      'text-color': [
+        {
+          text: [
+            'text-basic',
+            'text-basic-inverse',
+            'text-subtle',
+            'text-disabled',
+            'text-primary',
+            'text-secondary',
+            'text-tertiary',
+            'text-error',
+            'text-warning',
+            'text-success',
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export const cn = (...classes: ClassValue[]) => {
   return twMerge(clsx(classes));
