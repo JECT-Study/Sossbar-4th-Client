f027de9f4246eba3d63683c7450dc8bd489f70b3
hjkl <goni1653@hufs.ac.kr>
2026-05-03 15:05:58 +0900

    chore: api 명세를 기반으로 한  로그인 msw 설정

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,13 +1,7 @@
 import { authHandlers } from './auth';
-import { notificationsHandlers } from './notifications';
-import { projectsHandlers } from './projects';
-import { reviewsHandlers } from './reviews';
-import { usersHandlers } from './users';
 
-export const handlers = [
-  ...authHandlers,
-  ...usersHandlers,
-  ...projectsHandlers,
-  ...reviewsHandlers,
-  ...notificationsHandlers,
-];
+/**
+ * 로그인 목만 활성화 (MSW 작업 브랜치).
+ * 다른 도메인 핸들러는 필요 시 `users`, `projects` 등을 다시 합치면 됩니다.
+ */
+export const handlers = [...authHandlers];
