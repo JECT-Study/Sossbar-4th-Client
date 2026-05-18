589692b01aaa095ab021b6848125901d7b8924c5
d5ng <dong5ffice@gmail.com>
2026-05-18 23:53:25 +0900

    chore: 리팩토링된 코드 삭제

--- shared/lib/api/api-response.ts
+++
@@ -1,8 +0,0 @@
-export type ApiResponse<T> = T | { data: T; message?: string; meta?: unknown };
-
-export const unwrapApiResponse = <T>(res: ApiResponse<T>): T => {
-  if (res && typeof res === 'object' && 'data' in res) {
-    return (res as { data: T }).data;
-  }
-  return res as T;
-};
