1e329b9cb60f77c34871213bbcd04374529dbe05
d5ng <dong5ffice@gmail.com>
2026-05-22 02:12:06 +0900

    refactor: 백틱에서 single quotes로 변경

--- shared/components/label/label.tsx
+++ shared/components/label/label.tsx
@@ -11,7 +11,7 @@ interface Props extends ComponentProps<typeof LabelPrimitive.Root> {
 export const Label = ({ className, children, required, ...restProps }: Props) => {
   return (
     <LabelPrimitive.Root
-      className={cn(`text-text-subtle text-heading-xs flex items-center gap-1 font-bold`, className)}
+      className={cn('text-text-subtle text-heading-xs flex items-center gap-1 font-bold', className)}
       {...restProps}
     >
       {children}
