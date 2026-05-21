91fb383750c391e5f440ab09ad6222365ff6722f
d5ng <dong5ffice@gmail.com>
2026-05-22 01:49:45 +0900

    feat: radix ui에서 스타일과 프로젝트 구현사항에 맞게 래핑

---
+++ shared/components/label/label.tsx
@@ -0,0 +1,28 @@
+import type { ComponentProps } from 'react';
+
+import { Label as LabelPrimitive } from 'radix-ui';
+
+import { cn } from '@/shared/lib/cn';
+
+interface Props extends ComponentProps<typeof LabelPrimitive.Root> {
+  required?: boolean;
+}
+
+export const Label = ({ className, children, required, ...restProps }: Props) => {
+  return (
+    <LabelPrimitive.Root
+      className={cn(`text-text-subtle text-heading-xs flex items-center gap-1 font-bold`, className)}
+      {...restProps}
+    >
+      {children}
+      {required ? (
+        <>
+          <span className="text-text-tertiary text-heading-xs font-bold" aria-hidden="true">
+            *
+          </span>
+          <span className="sr-only">필수</span>
+        </>
+      ) : null}
+    </LabelPrimitive.Root>
+  );
+};
