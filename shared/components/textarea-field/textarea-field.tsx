9fdbc5560aabd03f5297128e40f7a12913234cf8
d5ng <dong5ffice@gmail.com>
2026-05-23 14:33:38 +0900

    feat: label, textarea, errormessage를 추상화한 컴포넌트 추가

---
+++ shared/components/textarea-field/textarea-field.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import type { ComponentProps } from 'react';
+
+import { cn } from '@/shared/lib/cn';
+
+import { ErrorMessage } from '../error-message';
+import { Label } from '../label';
+import { Textarea } from '../textarea/textarea';
+
+interface Props extends ComponentProps<'textarea'> {
+  name: string;
+  label: string;
+  required?: boolean;
+  errorMessage?: string;
+  className?: string;
+  textareaClassName?: string;
+}
+
+export const TextareaField = ({
+  name,
+  label,
+  required,
+  errorMessage,
+  className,
+  textareaClassName,
+  disabled,
+  maxLength,
+  value,
+  defaultValue,
+  ...restProps
+}: Props) => {
+  return (
+    <div className={cn('flex flex-col gap-2', className)}>
+      <Label required={required} htmlFor={`textarea-${name}`}>
+        {label}
+      </Label>
+      <div className="flex flex-col gap-2">
+        <Textarea
+          name={name}
+          id={`textarea-${name}`}
+          required={required}
+          disabled={disabled}
+          maxLength={maxLength}
+          value={value}
+          defaultValue={defaultValue}
+          aria-describedby={errorMessage ? `${name}-error-message` : undefined}
+          placeholder="내용을 입력해주세요."
+          className={textareaClassName}
+          error={!!errorMessage}
+          {...restProps}
+        />
+        {errorMessage ? <ErrorMessage id={`${name}-error-message`}>{errorMessage}</ErrorMessage> : null}
+      </div>
+    </div>
+  );
+};
