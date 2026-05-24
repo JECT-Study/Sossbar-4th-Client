0560a01320c45c7b4cc96692b72388f2d57c6ce0
sohyoeun <she020108@gmail.com>
2026-05-24 13:56:49 +0900

    fix: 프로필 예시 페이지 타입 에러 해결

--- app/profile-examples/page.tsx
+++ app/profile-examples/page.tsx
@@ -1,6 +1,7 @@
 import { ProfilePageContent } from '@/features/profile';
 
-const DEMO_PROFILE_USER_ID = 'example';
+// 추후 정적 예시 데이터 기반으로 재구현 예정
+const DEMO_PROFILE_USER_ID = 0;
 
 const ProfileExamplesPage = () => {
   return <ProfilePageContent userId={DEMO_PROFILE_USER_ID} isMyProfile={false} />;
