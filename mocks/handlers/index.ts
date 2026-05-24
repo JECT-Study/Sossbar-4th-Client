f02fe06fd3c798b720c28d910c4ad6eec8ce7069
sohyoeun <she020108@gmail.com>
2026-05-24 14:09:32 +0900

    fix: Lint 에러 해결

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,8 +1,13 @@
 import { authHandlers } from './auth';
-
 import { profileHandlers } from './profile';
 import { projectsHandlers } from './projects';
 import { reviewsHandlers } from './reviews';
 import { usersHandlers } from './users';
 
-export const handlers = [...authHandlers, ...usersHandlers, ...profileHandlers, ...projectsHandlers, ...reviewsHandlers];
+export const handlers = [
+  ...authHandlers,
+  ...usersHandlers,
+  ...profileHandlers,
+  ...projectsHandlers,
+  ...reviewsHandlers,
+];
