f8d613b31c3162c3ae3d929d49fd8e82235e496c
sohyoeun <she020108@gmail.com>
2026-05-26 00:25:58 +0900

    fix: export 오타 수정

--- features/review/index.ts
+++ features/review/index.ts
@@ -2,13 +2,7 @@ export { ReviewSubmitDialog } from './components/review-submit-dialog';
 export { ReviewWriteContent } from './components/review-write-content';
 export { useCreateReview } from './mutations';
 export { reviewKeys } from './query-keys';
-export {
-  useReceivedTagsQuery,
-  useReviewFormData,
-  useReviews,
-  useSpectrumStats,
-  useSpectrumStatsByProject,
-} from './queries';
+export { useReceivedTags, useReviewFormData, useReviews, useSpectrumStats, useSpectrumStatsByProject } from './queries';
 export { ReviewListCard } from './components/review-list-card';
 export { SoftSkillsCard } from './components/softskills-card';
 export { TagCard } from './components/tag-card';
