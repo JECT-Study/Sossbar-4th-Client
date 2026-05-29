2bf36947841543e1f8db298b1467907af5006b26
hjkl <goni1653@hufs.ac.kr>
2026-05-29 13:40:44 +0900

    fix: 크래시 원인으로 인한 탈퇴오류 수정

--- features/mypage/hooks/use-delete-account.mutation.ts
+++ features/mypage/hooks/use-delete-account.mutation.ts
@@ -1,17 +1,14 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 import { clearSessionUser } from '@/shared/lib/session-user';
 
 import { deleteAccount } from '../apis/delete-account.api';
 
 export const useDeleteAccountMutation = () => {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: deleteAccount,
     onSuccess: () => {
       clearSessionUser();
-      queryClient.clear();
     },
   });
 };
