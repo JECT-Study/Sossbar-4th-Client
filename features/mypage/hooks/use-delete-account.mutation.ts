54f85f814251e7e59fc54b42b042504ddcc1c7e2
hjkl <goni1653@hufs.ac.kr>
2026-05-29 14:00:35 +0900

    fix: Application error 로그인 온보딩 문제 개선

--- features/mypage/hooks/use-delete-account.mutation.ts
+++ features/mypage/hooks/use-delete-account.mutation.ts
@@ -1,16 +1,9 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { clearAuthToken } from '@/shared/lib/auth-token';
-import { clearSessionUser } from '@/shared/lib/session-user';
-
 import { deleteAccount } from '../apis/delete-account.api';
 
 export const useDeleteAccountMutation = () => {
   return useMutation({
     mutationFn: deleteAccount,
-    onSuccess: () => {
-      clearSessionUser();
-      clearAuthToken();
-    },
   });
 };
