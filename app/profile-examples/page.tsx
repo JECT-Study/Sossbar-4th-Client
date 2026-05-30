c8a9d56e2d94a21cbcab4fe6898e6b9709581c77
sohyoeun <she020108@gmail.com>
2026-05-30 21:23:51 +0900

    fix: 프로필 예시 페이지를 타인의 프로필 보는 시점으로 수정

--- app/profile-examples/page.tsx
+++ app/profile-examples/page.tsx
@@ -3,7 +3,7 @@ import { ProfilePageContent } from '@/features/profile';
 const DEMO_PROFILE_USER_ID = 1;
 
 const ProfileExamplesPage = () => {
-  return <ProfilePageContent userId={DEMO_PROFILE_USER_ID} isMyProfile />;
+  return <ProfilePageContent userId={DEMO_PROFILE_USER_ID} isMyProfile={false} />;
 };
 
 export default ProfileExamplesPage;
