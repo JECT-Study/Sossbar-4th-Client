72de267128836240a597e95b487b96e7ac3f80ca
d5ng <dong5ffice@gmail.com>
2026-05-31 14:45:47 +0900

    feat: 프로필 관련 hooks 및 쿼리 키 추가

--- features/profile/index.ts
+++ features/profile/index.ts
@@ -1,5 +1,6 @@
-export { useUpdateProfile } from './mutations';
-export { useProfile } from './queries';
+export { useUpdateProfile } from './hooks/use-update-profile.mutation';
+export { useProfileById } from './hooks/use-profile-by-id.query';
+export { useMyProfile } from './hooks/use-my-profile.query';
 export { ProfileAvatar } from './components/profile-avatar';
 export { ProfilePageContent } from './components/profile-page-content';
 export { ProfileSection } from './components/profile-section';
