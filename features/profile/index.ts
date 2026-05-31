413c26858ddff2ba64dcf01cd29800f229039d7e
d5ng <dong5ffice@gmail.com>
2026-05-31 16:08:35 +0900

    feat: barrel pattern 수정

--- features/profile/index.ts
+++ features/profile/index.ts
@@ -1,8 +1,9 @@
-export { useUpdateProfile } from './hooks/use-update-profile.mutation';
-export { useProfileById } from './hooks/use-profile-by-id.query';
+export { useIsMyProfile } from './hooks/use-is-my-profile';
 export { useMyProfile } from './hooks/use-my-profile.query';
+export { useProfileById } from './hooks/use-profile-by-id.query';
+export { useUpdateProfile } from './hooks/use-update-profile.mutation';
 export { ProfileAvatar } from './components/profile-avatar';
-export { ProfilePageContent } from './components/profile-page-content';
 export { ProfileSection } from './components/profile-section';
 export { ProjectSection } from './components/project-section';
+export { ProfileSectionBoundary } from './components/profile-section-boundary';
 export type { Profile, UpdateProfilePayload } from './types';
