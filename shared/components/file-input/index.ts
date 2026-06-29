0beb28ffd7b4f52dd85aa8eecb88de7a78614d47
sohyoeun <she020108@gmail.com>
2026-06-29 17:20:06 +0900

    chore: useFileInput export 추가

--- shared/components/file-input/index.ts
+++ shared/components/file-input/index.ts
@@ -2,4 +2,5 @@ export { FileInput } from './file-input';
 export type { FileInputProps } from './file-input';
 export { ImageFileInput } from './image-file-input';
 export type { ImageFileInputProps } from './image-file-input';
+export { useFileInput } from './use-file-input';
 export { useImagePreview } from './use-image-preview';
